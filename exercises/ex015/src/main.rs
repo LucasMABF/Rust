66f57ed991da30a9368d2e7dbc18cd28cae75b84
Lucas Mendes <70616909+LucasMABF@users.noreply.github.com>
2023-01-29 23:23:31 -0300

    added exercise
    added Exercise and examples about hash maps, explained on rust's book.

---
+++ exercises/ex015/src/main.rs
@@ -0,0 +1,53 @@
+use std::collections::HashMap; // HashMaps are not automatically in scope on root
+
+fn main() {
+    // HashMaps
+    // Hashmaps is a collection that connects a key and a value, just like Dictionaries in python
+    // they are of type HashMap<K, V).
+    let mut scores:HashMap<String, i32> = HashMap::new();
+
+    scores.insert(String::from("Blue Team"), 10);
+    scores.insert(String::from("Yellow Team"), 50);
+
+    println!("{:?}", scores);
+    // get value from key
+    println!("Blue Team score: {}", scores.get("Blue Team").copied().unwrap_or(0));
+
+    
+    let new_team = String::from("Red Team");
+    let new_team_score = 100;
+    scores.insert(new_team, new_team_score); // takes ownership of String and copies i32
+    // println!("{new_team}"); can't value moved
+    println!("{new_team_score}"); // we could creae a hashmap with references too, with type &String or &i32.
+
+    // iterate
+    for (key, value) in &scores{ // without & scores moved and invalid after
+        println!("{key}: {value}");
+    }
+
+    // updating hashmap
+
+    scores.insert(String::from("Red Team"), 80); // overwrites old value;
+
+    scores.entry(String::from("Blue Team")).or_insert(20); // only inserts if there wasn't a value before
+    scores.entry(String::from("Green Team")).or_insert(120);
+
+    let blue_team_score_change = scores.entry(String::from("Blue Team")).or_insert(20); // returns a mutable reference to the value
+
+    *blue_team_score_change += 200;
+
+    for (key, value) in &scores{ // without & scores moved and invalid after
+        println!("{key}: {value}");
+    }
+
+    let text = "hello how are you hello you how how how hi";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace(){
+        let count = map.entry(word).or_insert(0);
+        *count +=1;
+    }
+    for (key, value) in &map{
+        println!("{key}: {value}");
+    }
+}
